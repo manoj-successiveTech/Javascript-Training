7dc5db38f3f8e8d825cebc2fa12712df53d956ff
Manoj Kumar <manoj.kumar@successive.tech>
2025-06-13 14:28:19 +0530

    assignment-2 updated

---
+++ Assignment 2/program7.js
@@ -0,0 +1,38 @@
+// 7. All of the above questions from 3-6 needs to be done with arrow functions also
+
+//3. Write a function expression that takes in another function as an argument using arrow function
+
+const firstName = () => "Manoj";     // Arrow function for firstName
+const fullName = (fname) => fname + " Kumar";   // Arrow function for fullName
+
+const myName = fullName(firstName());  // Get the full name by passing firstName() as an argument
+
+console.log("The output of name is::" ,myName);  //Output
+
+
+// 4. Write a function expression that takes in a number and returns its square using arrow function syntax.
+
+const squareNum = (num) => num ** 2;
+let square = squareNum(5);    // Calling the function with an example number (e.g - 5)
+
+console.log("The square of the number is:", square);   // Logging the result
+
+//5. Using Arrow function to calculate the sum of two numbers
+
+const sumNum = (num1, num2) => num1 + num2;
+
+let sum = sumNum(10, 20);                       // Calculate the sum
+console.log("The sum of the numbers is:", sum);  // Log the result
+
+
+//6. Arrow function to check if a number is even
+
+const checkEven = (num) => num % 2 === 0;
+let num = 5;
+
+if (checkEven(num)) {
+    console.log("The number is Even");
+} else {
+    console.log("The number is Odd");
+}
+
--- Assignment-4/program4.js
+++
@@ -1,9 +0,0 @@
-// 4. Write a program to convert given string to upperCase OR lowerCase.
-
-function convertCase(str, toUpper) 
-{
-  return toUpper ? str.toUpperCase() : str.toLowerCase();
-}
-
-console.log("The output upperCase is ::" ,convertCase("Hello World", true));  // Output: "HELLO WORLD"
-console.log("The output lowercase is ::" ,convertCase("Hello World", false)); // Output: "hello world"
